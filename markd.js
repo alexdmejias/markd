3384e66908384cb4709b18af4c70616833d67001
Alex Mejias <alexdmejias@gmail.com>
2015-02-14 00:57:33 -0500

    added ability to edit

--- markd.js
+++ markd.js
@@ -8,6 +8,18 @@ if (Meteor.isClient) {
     list: function () {
       var author = Meteor.userId();
       return MarksList.find({createdBy: author}, {sort :{date: -1}})
+    },
+    editing: function() {
+      return Session.equals('editing', this._id);
+    },
+    editingStatus: function() {
+      var label = '';
+      if (Session.equals('editing', this._id)) {
+        label = 'save';
+      } else {
+        label = 'edit';
+      }
+      return label;
     }
   });
 
@@ -25,15 +37,23 @@ if (Meteor.isClient) {
   });
 
   Template.list.events({
-    // 'click .edit': function() {
-    //   this.link = false;
-    //   console.log(this)
-    //
-    // },
+    'click .edit': function(event, template) {
+      var currentVal = Session.get('editing');
+      if (currentVal !== null) {
+        Session.set('editing', null);
+      }
+
+      if (currentVal === this._id) {
+        var inputValue = template.find('#editField-' + this._id).value;
+        var isLink = (inputValue.substr(0,4) === 'http') ? true : false;
+        MarksList.update(this._id, {$set: {text: inputValue, link: isLink}});
+      } else {
+        Session.set('editing', this._id);
+      }
+    },
     'click .remove': function() {
       MarksList.remove(this._id);
     }
-
   })
 }
 
