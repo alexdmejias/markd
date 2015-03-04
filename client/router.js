7515c19355ce3aae0b48186b9d8d8d359d8c63ba
Alex Mejias <alexdmejias@gmail.com>
2015-03-03 21:45:15 -0500

    Many a changes:
    - ability to archive marks
    - added icons to action buttons
    - ui changes
    - simple nav
    - cleaned up code
    - started progress on the tagging ability

--- client/router.js
+++ client/router.js
@@ -2,20 +2,28 @@
 
 Router.configure({
   layoutTemplate: 'layout'
-})
+});
 
 Router.route('/', function() {
-  var marks = MarksList.find();
-  // this.layout('layout');
-  this.render('overview', {data: {marks: marks}});
+  var marks = MarksList.find({createdBy: Meteor.userId(), archived: false}, {sort :{date: -1}})
+  this.render('overview', { data: { records: marks } });
 }, {
   name: 'overview'
 });
 
+Router.route('/marks/archive', function() {
+  var marks = MarksList.find({createdBy: Meteor.userId(), archived: true}, {sort :{date: -1}})
+  this.render('list', { data: { records: marks } } );
+  }, {
+    name: 'archive'
+  }
+);
+
 Router.route('/marks/:_id', function () {
   var mark = MarksList.findOne({_id: this.params._id});
 
-  if ((Meteor.userId() === mark.createdBy)) {
+  if ((Meteor.userId() === mark.createdBy) ||
+      ((typeof(mark.private) !== "undefined") && (mark.private === false))) {
     this.render('single', {data: mark});
   }
 });
--- markd.js
+++ markd.js
@@ -1,14 +1,12 @@
 MarksList = new Mongo.Collection('marks');
 
 if (Meteor.isClient) {
-  // counter starts at 0
   Session.set('editing', null);
 
   Template.list.helpers({
-    list: function () {
-      var author = Meteor.userId();
-      return MarksList.find({createdBy: author}, {sort :{date: -1}})
-    },
+  });
+
+  Template.single.helpers({
     editing: function() {
       return Session.equals('editing', this._id);
     },
@@ -20,6 +18,15 @@ if (Meteor.isClient) {
         label = 'edit';
       }
       return label;
+    },
+    displayDate: function() {
+      return moment(this.date).format('MM/DD/YYYY, HH:MM');
+    },
+    inSingle: function() {
+      return (Router.current().route.getName() === 'overview') ? false : true;
+    },
+    isArchived: function() {
+      return this.archived;
     }
   });
 
@@ -31,12 +38,16 @@ if (Meteor.isClient) {
         text: inputValue,
         date: new Date(),
         createdBy: Meteor.userId(),
-        link: isLink
+        link: isLink,
+        title: null,
+        archived: false,
+        private: true,
+        tags: []
       })
     }
   });
 
-  Template.list.events({
+  Template.single.events({
     'click .edit': function(event, template) {
       var currentVal = Session.get('editing');
       if (currentVal !== null) {
@@ -45,16 +56,41 @@ if (Meteor.isClient) {
 
       if (currentVal === this._id) {
         var inputValue = template.find('#editField-' + this._id).value;
+        var titleValue = template.find('#editTitleField-' + this._id).value;
+
         var isLink = (inputValue.substr(0,4) === 'http') ? true : false;
-        MarksList.update(this._id, {$set: {text: inputValue, link: isLink}});
+        MarksList.update(this._id, {$set: {text: inputValue, link: isLink, title: titleValue}});
       } else {
         Session.set('editing', this._id);
       }
     },
     'click .remove': function() {
       MarksList.remove(this._id);
+    },
+
+    'click ._tags': function(event) {
+      console.log(event);
+    },
+
+    'click .archive': function() {
+      var status = this.archived || false;
+      MarksList.update(this._id, {$set: {archived: !status}});
+    },
+
+    'blur ._tags': function(event) {
+      console.log('blurred the field', event);
+      var inputVal = event.target.tags.value;
+
+      var tags = inputVal.split(',');
+
+
     }
-  })
+
+    // 'click ._j_archive': function() {
+    //   var archivedValue = true
+    //   MarksList.update(this._id, {$set: {archived: }});
+    // }
+  });
 }
 
 if (Meteor.isServer) {
