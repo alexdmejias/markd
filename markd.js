4508ffd946c80b6fd9bf3ffcac9337b352aaba60
Alex Mejias <alexdmejias@gmail.com>
2015-03-14 16:16:53 -0400

    javascript tags progress and moved the single helpers into the its appropriate place

--- markd.js
+++ markd.js
@@ -2,13 +2,34 @@ MarksList = new Mongo.Collection('marks');
 
 if (Meteor.isClient) {
   Session.set('editing', null);
+  Session.set('editingTags', null);
+
+  Meteor.startup(function () {
+    $(document).on('keydown', function (e) {
+      if(e.keyCode === 13 && e.metaKey) {
+        var $mainInputForm = $('.mainInputForm');
+        if ($mainInputForm.length && $mainInputForm.find('textarea').val()) {
+          $mainInputForm.submit();
+        }
+    	}
+    });
+  });
 
   Template.list.helpers({
   });
 
-  Template.single.helpers({
-    editing: function() {
-      return Session.equals('editing', this._id);
+  Template.single_header.helpers({
+    displayDate: function() {
+      return moment(this.date).format('MM/DD/YYYY, HH:MM');
+    },
+    inSingle: function() {
+      return (Router.current().route.getName() !== 'overview');
+    },
+    isArchived: function() {
+      return this.archived;
+    },
+    editingTags: function() {
+      return Session.equals('editingTags', this._id);
     },
     editingStatus: function() {
       var label = '';
@@ -18,22 +39,20 @@ if (Meteor.isClient) {
         label = 'edit';
       }
       return label;
-    },
-    displayDate: function() {
-      return moment(this.date).format('MM/DD/YYYY, HH:MM');
-    },
-    inSingle: function() {
-      return (Router.current().route.getName() === 'overview') ? false : true;
-    },
-    isArchived: function() {
-      return this.archived;
     }
+  });
+
+  Template.single.helpers({
+    editing: function() {
+      return Session.equals('editing', this._id);
+    },
+
   });
 
   Template.input.events({
     'submit form': function(event) {
       var inputValue = event.target.mainInput.value;
-      var isLink = (inputValue.substr(0,4) === 'http') ? true : false;
+      var isLink = (inputValue.substr(0,4) === 'http');
       MarksList.insert({
         text: inputValue,
         date: new Date(),
@@ -58,7 +77,7 @@ if (Meteor.isClient) {
         var inputValue = template.find('#editField-' + this._id).value;
         var titleValue = template.find('#editTitleField-' + this._id).value;
 
-        var isLink = (inputValue.substr(0,4) === 'http') ? true : false;
+        var isLink = (inputValue.substr(0,4) === 'http');
         MarksList.update(this._id, {$set: {text: inputValue, link: isLink, title: titleValue}});
       } else {
         Session.set('editing', this._id);
@@ -68,28 +87,40 @@ if (Meteor.isClient) {
       MarksList.remove(this._id);
     },
 
-    'click ._tags': function(event) {
-      console.log(event);
-    },
+    //'click ._tags': function(event) {
+    //  console.log(event);
+      //Session.set('editingTitle', this._id);
+    //},
 
     'click .archive': function() {
       var status = this.archived || false;
       MarksList.update(this._id, {$set: {archived: !status}});
     },
 
-    'blur ._tags': function(event) {
-      console.log('blurred the field', event);
-      var inputVal = event.target.tags.value;
+    'click .tag': function() {
+      Session.set('editingTags', this._id);
+    },
 
-      var tags = inputVal.split(',');
+    'blur .tags_field': function(event) {
+      var tags = event.target.value.split(',');
 
-
+      //var currentTags = this.tags;
+      var newTags = [];
+      if (tags.length > 0) {
+        $.each(tags, function(index, tag) {
+          tag = $.trim(tag);
+          if ((tag.length) && (typeof tag === "string")) {
+            newTags.push(tag);
+          }
+        });
+      }
+      MarksList.update(this._id, {$set: {tags: newTags}});
+    },
+    'click .showModal': function() {
+      console.log('wasd');
+      $('.ui.modal').modal('show');
     }
 
-    // 'click ._j_archive': function() {
-    //   var archivedValue = true
-    //   MarksList.update(this._id, {$set: {archived: }});
-    // }
   });
 }
 
