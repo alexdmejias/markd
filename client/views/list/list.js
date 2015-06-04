eb9122269bf3d71e6d3ffd6059ca270e7567a5bf
Alex Mejias <alexdmejias@gmail.com>
2015-06-03 23:13:45 -0400

    feat: added tag cloud

--- client/views/list/list.js
+++ client/views/list/list.js
@@ -1,2 +1,6 @@
 Template.list.helpers({
+	tags: function() {
+		var validTags = _.filter(_.flatten(_.pluck(MarksList.find().fetch(), 'tags')));
+		return _.uniq(validTags,function(item){return JSON.stringify(item);});
+	}
 });
