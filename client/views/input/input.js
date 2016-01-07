7707a4fdd4079b4ed2f6efeedb46cfbcc4dc3c36
Alex Mejias <alexdmejias@gmail.com>
2016-01-07 00:04:46 -0500

    removed old tags functionality

--- client/views/input/input.js
+++ client/views/input/input.js
@@ -23,24 +23,7 @@ Template.input.events({
 });
 
 Template.input.helpers({
-	'tags': function() {
-		var validTags = _.filter( _.sortBy( _.flatten( _.pluck(MarksList.find().fetch(), 'tags'), 'name' ) ).reverse() , 'name');
-		var newTags = [];
-		_.forEach(validTags, function(tag, index) {
-			var filtered = _.find(newTags, {'name': tag.name});
-			if (filtered) {
-				var filteredIndex = _.findIndex(newTags, {name: tag.name});
-				newTags[filteredIndex].count += 1;
-			} else {
-				tag.count = 1;
-				newTags.push(tag);
-			}
-		});
-
-		return newTags;
-	}
 });
-
 
 Template.input.onRendered(function() {
 	$('.ui.dropdown').dropdown({allowAdditions: true });
--- client/views/single/_single_footer.js
+++ client/views/single/_single_footer.js
@@ -4,42 +4,5 @@ Template.single_footer.helpers({
   }
 });
 Template.single_footer.events({
-  'click .__tags_add': function (e) {
-    $(e.target).parent().next().addClass('adding');
-  },
-
-  'click .__tags_save': function(e) {
-    var input = $(e.currentTarget).prev(),
-      value = input.val();
-
-    var currTags = MarksList.findOne({_id: this._id}).tags;
-
-    if (value !== '') {
-      var tags = value.split(',');
-      _.each(tags, function(tag) {
-        if (tag) {
-          var insert = {
-            color: '',
-            slug: slugify(tag),
-            name: tag
-          };
-          if (_.filter(currTags, {'name': insert.name, 'slug': insert.slug}).length === 0) {
-            MarksList.update(this._id, {$push: {tags: insert}});
-          } else {
-            sAlert.error(`This Mark already has the '${insert.name}' tag.`);
-          }
-        }
-      });
-      input.val('');
-    }
-    // hide the tag box
-    $(e.currentTarget).parent().removeClass('adding');
-  },
-
-  'click .__tags_delete': function(e, template) {
-    e.preventDefault();
-    var tags = MarksList.findOne({_id: template.data._id}).tags;
-    var newTags = _.reject(tags, {'slug': this.slug, 'name': this.name});
-    MarksList.update(template.data._id, {$set: {tags: newTags}});
-  }
+  
 });
