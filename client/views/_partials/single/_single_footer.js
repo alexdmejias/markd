6b6770b9c3da7e1a5fafd2a1bcc80afc60b252f6
Alex Mejias <alexdmejias@gmail.com>
2016-01-11 00:53:36 -0500

    post tags are now displayed

--- client/views/_partials/single/_single_footer.js
+++ client/views/_partials/single/_single_footer.js
@@ -1,8 +1,17 @@
 Template.single_footer.helpers({
-  editingTags: function () {
-    return Session.equals('editingTags', this._id);
+  'tagsObj': function() {
+    var self = this;
+    var tags = [];
+
+    if (!self.tags || _.isEmpty(self.tags)) return false;
+
+    Tags.find({'_id': {$in: self.tags}}).forEach(function(doc) {
+      tags.push(doc)
+    });
+
+    return tags;
   }
 });
 Template.single_footer.events({
-  
+
 });
